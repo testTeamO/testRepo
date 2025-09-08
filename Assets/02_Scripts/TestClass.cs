04698cf083d1a9e97cdad76953e771d7903a4f5a
endsun1234 <endsun1234@naver.com>
2025-09-08 15:05:25 +0900

    깃허브 테스트하기
    TestClass를 생성하여 임시 변수와 임시 함수를 만들고 Test 게임오브젝트에 스크립트를 컴포넌트로 추가하여 작동하는지 확인함.

---
+++ Assets/02_Scripts/TestClass.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 테스트 용 클래스입니다.
+/// </summary>
+public class TestClass : MonoBehaviour
+{
+    [SerializeField] int _testInt;
+    [SerializeField] string _textString;
+
+    private void Start()
+    {
+        PrintNumber(_testInt);
+        PrintText(_textString);
+    }
+
+    /// <summary>
+    /// 테스트용으로 입력받은 숫자를 출력하는 함수
+    /// </summary>
+    /// <param name="number">출력할 숫자</param>
+    public void PrintNumber(int number)
+    {
+        Debug.Log($"숫자 출력하기 : {number}");
+    }
+
+    /// <summary>
+    /// 테스트용으로 입력받은 텍스트를 출력하는 함수
+    /// </summary>
+    /// <param name="text">출력할 텍스트</param>
+    public void PrintText(string text)
+    {
+        Debug.Log($"텍스트 출력하기 : {text}");
+    }
+}
