18a1996ed0f437e68aeaefb160f3a3ab10760d1c
whtjdgh1931 <pmkjo1931@gmail.com>
2025-09-08 17:08:41 +0900

    feat : test
    추가 테스트 오브젝트

--- Assets/whtjdgh1931/Scripts/TestMono.cs
+++ Assets/whtjdgh1931/Scripts/TestMono.cs
@@ -2,15 +2,8 @@ using UnityEngine;
 
 public class TestMono : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Ãß°¡ Test");
     }
 }
